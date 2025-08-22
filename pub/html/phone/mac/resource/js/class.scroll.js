1a71b4d80d4fa26bc70f5d2c674aaa8ddbaebf0f
yu_ha_k <kny1320@gmail.com>
2025-08-22 09:19:19 +0900

    chore: consolidate to /docs only; remove root /pub

--- pub/html/phone/mac/resource/js/class.scroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 10) {
-        $("header.tr").removeClass("transp");
-    } else {
-        $("header.tr").addClass("transp");
-    }
-});
- 
